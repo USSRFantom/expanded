9551ce560020344d1e9a8bc058a600af46260d1e
UssrFantom <dimkakrohin@mail.ru>
2021-10-14 12:24:50 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class Myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Expanded'),
+          centerTitle: true,
+        ),
+        body: ,
+      ),
+    );
+  }
+
+}
